7465215cd57ffb0c10bbc2740e9d8fa9291ae112
Md Sakibul Islam <sakibul.islam0808@gmail.com>
2024-11-10 21:34:40 +0600

    Add category creation page with form handling and styling updates

---
+++ app/(private)/(with-sidebar)/super-admin/category/add/page.jsx
@@ -0,0 +1,145 @@
+"use client";
+
+import FormInput from "@/components/form/form-input";
+import Label from "@/components/shared/label";
+import Title from "@/components/shared/title";
+import { Breadcrumb, Button, Cascader } from "antd";
+import Dragger from "antd/es/upload/Dragger";
+import { useFormik } from "formik";
+import { ImageUp } from "lucide-react";
+import Link from "next/link";
+
+const options = [
+  {
+    value: "zhejiang",
+    label: "Zhejiang",
+    children: [
+      {
+        value: "hangzhou",
+        label: "Hanzhou",
+        children: [
+          {
+            value: "xihu",
+            label: "West Lake",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    value: "jiangsu",
+    label: "Jiangsu",
+    children: [
+      {
+        value: "nanjing",
+        label: "Nanjing",
+        children: [
+          {
+            value: "zhonghuamen",
+            label: "Zhong Hua Men",
+          },
+        ],
+      },
+    ],
+  },
+];
+
+const items = [
+  {
+    title: <Link href="/super-admin/dashboard">Dashboard</Link>,
+  },
+  {
+    title: <Link href="/super-admin/category">Category</Link>,
+  },
+  {
+    title: "Add Category",
+  },
+];
+
+export default function AddCategory() {
+  const formik = useFormik({
+    initialValues: {
+      parent_category_id: "",
+      name: "",
+      route: "",
+      image: "",
+    },
+  });
+
+  const onChange = (value) => {
+    console.log(value);
+  };
+  return (
+    <div className="space-y-5 lg:space-y-10">
+      <Breadcrumb items={items} />
+
+      <div className="space-y-3 sm:rounded-md sm:bg-white sm:p-6 sm:shadow lg:mx-auto lg:w-8/12 lg:p-8">
+        <Title title={"Create New Category"} />
+        <form onSubmit={formik.handleSubmit} className="space-y-5">
+          <div className="space-y-2">
+            <div className="flex flex-col gap-1">
+              <Label htmlFor="parent_category_id" className={"py-1"}>
+                Parent Category (If any)
+              </Label>
+              <Cascader
+                options={options}
+                onChange={onChange}
+                changeOnSelect
+                className="!w-full"
+              />
+            </div>
+
+            <div className="grid grid-cols-1 gap-x-4 gap-y-2 sm:grid-cols-2">
+              <FormInput
+                label="Name"
+                name="name"
+                placeholder="Enter category name"
+                formik={formik}
+                required
+              />
+              <FormInput
+                label="Category URL"
+                name="route"
+                placeholder="Enter category url"
+                formik={formik}
+                required
+              />
+            </div>
+
+            <div className="space-y-1">
+              <Label htmlFor={"image"}>Category Image</Label>
+              <Dragger
+                maxCount={1}
+                multiple={false}
+                accept=".jpg,.jpeg,.png"
+                onChange={({ file }) => {
+                  formik.setFieldValue("image", file?.originFileObj);
+                }}
+                fileList={formik.values.file ? [formik.values.file] : []}
+              >
+                <p className="flex justify-center">
+                  <ImageUp className="size-8 opacity-70" />
+                </p>
+                <p className="ant-upload-text !mt-3">
+                  Click or drag file to this area to upload
+                </p>
+                <p className="ant-upload-hint !text-sm">
+                  Support only .jpg, .jpeg, .png file format.
+                </p>
+              </Dragger>
+            </div>
+          </div>
+
+          <Button
+            type="primary"
+            htmlType="submit"
+            className="w-full"
+            // loading={isLoading}
+          >
+            Create Category
+          </Button>
+        </form>
+      </div>
+    </div>
+  );
+}
