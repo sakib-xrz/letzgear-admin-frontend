376506085c2efa23761afa39d05b62b87fb60e3d
Md Sakibul Islam <sakibul.islam0808@gmail.com>
2024-11-14 12:09:10 +0600

    Remove commented-out profile dropdown option from constants

--- utils/constant.js
+++ utils/constant.js
@@ -14,17 +14,17 @@ export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 export const generateProfileDropdownOptions = (role) => {
   return [
-    {
-      key: `/${role}/dashboard/profile`,
-      label: (
-        <Link
-          href={`/${role}/dashboard/profile`}
-          className="flex items-center gap-2"
-        >
-          <UsersRound className="size-5" /> Profile
-        </Link>
-      ),
-    },
+    // {
+    //   key: `/${role}/dashboard/profile`,
+    //   label: (
+    //     <Link
+    //       href={`/${role}/dashboard/profile`}
+    //       className="flex items-center gap-2"
+    //     >
+    //       <UsersRound className="size-5" /> Profile
+    //     </Link>
+    //   ),
+    // },
     {
       key: "/change-password",
       label: (
