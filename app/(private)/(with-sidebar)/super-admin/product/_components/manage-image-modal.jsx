0d12b7afd0eda802b3e1a5757f107f01335f745d
Md Sakibul Islam <sakibul.islam0808@gmail.com>
2024-11-20 02:40:04 +0600

    Refactor Manage Image modal layout and update image upload dimensions

--- app/(private)/(with-sidebar)/super-admin/product/_components/manage-image-modal.jsx
+++ app/(private)/(with-sidebar)/super-admin/product/_components/manage-image-modal.jsx
@@ -14,7 +14,7 @@ export default function ManageImageModal({
       open={openManageImagesModal}
       onCancel={() => setOpenManageImagesModal(false)}
       title={
-        <div className="pr-5">
+        <div className="pr-8">
           Manage Images for {currentProduct?.name}{" "}
           <span className="font-normal text-gray-500">
             ({currentProduct?.sku})
@@ -39,7 +39,7 @@ export default function ManageImageModal({
                   alt=""
                   width={1080}
                   height={1080}
-                  className="size-40 rounded object-cover"
+                  className="size-32 rounded object-cover"
                 />
                 <Button
                   size="small"
@@ -92,7 +92,7 @@ export default function ManageImageModal({
                   alt=""
                   width={1080}
                   height={1080}
-                  className="size-40 rounded object-cover"
+                  className="size-32 rounded object-cover"
                 />
                 <Button
                   size="small"
@@ -136,54 +136,57 @@ export default function ManageImageModal({
         </div>
 
         <div className="mt-4">
-          <div>
-            <Label className={"mb-2"}>
-              Extra Images <span className="text-gray-500">(max 4)</span>
-            </Label>
-            <Dragger
-              maxCount={4}
-              multiple={false}
-              accept=".jpg,.jpeg,.png"
-              onChange={({ file }) => {
-                // formik.setFieldValue("image", file?.originFileObj);
-              }}
-              fileList={[]}
-              className="w-full"
-            >
-              <p className="flex justify-center">
-                <ImageUp className="size-8 opacity-70" />
-              </p>
-              <p className="ant-upload-text !mt-3">
-                Click or drag file to this area to upload
-              </p>
-              <p className="ant-upload-hint !text-sm">
-                Support only .jpg, .jpeg, .png file format.
-              </p>
-            </Dragger>
-          </div>
-          <div className="mt-4 grid w-full grid-cols-2 items-center gap-4 sm:grid-cols-4">
-            {[...Array(4).keys()].map((_, index) => (
-              <div className="relative w-full sm:w-fit" key={index}>
-                <Image
-                  src={
-                    "https://res.cloudinary.com/dl5rlskcv/image/upload/v1732000963/default-product_ilbqau.jpg"
-                  }
-                  alt=""
-                  width={1080}
-                  height={1080}
-                  className="w-full rounded object-cover sm:size-32"
-                />
-                <Button
-                  size="small"
-                  shape="circle"
-                  className="!absolute right-2 top-2"
-                  danger
-                >
-                  <Trash2 size={16} />
-                </Button>
-              </div>
-            ))}
-          </div>
+          <Label className={"mb-2"}>
+            Extra Images <span className="text-gray-500">(max 4)</span>
+          </Label>
+          {false ? (
+            <div className="mt-4 grid w-full grid-cols-2 items-center gap-4 sm:grid-cols-4">
+              {[...Array(4).keys()].map((_, index) => (
+                <div className="relative w-full sm:w-fit" key={index}>
+                  <Image
+                    src={
+                      "https://res.cloudinary.com/dl5rlskcv/image/upload/v1732000963/default-product_ilbqau.jpg"
+                    }
+                    alt=""
+                    width={1080}
+                    height={1080}
+                    className="w-full rounded object-cover sm:size-32"
+                  />
+                  <Button
+                    size="small"
+                    shape="circle"
+                    className="!absolute right-2 top-2"
+                    danger
+                  >
+                    <Trash2 size={16} />
+                  </Button>
+                </div>
+              ))}
+            </div>
+          ) : (
+            <div>
+              <Dragger
+                maxCount={4}
+                multiple={false}
+                accept=".jpg,.jpeg,.png"
+                onChange={({ file }) => {
+                  // formik.setFieldValue("image", file?.originFileObj);
+                }}
+                fileList={[]}
+                className="w-full"
+              >
+                <p className="flex justify-center">
+                  <ImageUp className="size-8 opacity-70" />
+                </p>
+                <p className="ant-upload-text !mt-3">
+                  Click or drag file to this area to upload
+                </p>
+                <p className="ant-upload-hint !text-sm">
+                  Support only .jpg, .jpeg, .png file format.
+                </p>
+              </Dragger>
+            </div>
+          )}
         </div>
       </>
     </Modal>
