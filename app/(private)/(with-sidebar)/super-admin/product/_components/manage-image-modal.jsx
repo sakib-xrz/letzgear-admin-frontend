604a74179879f2cdb52f5c188b47da5002a2fccb
Md Sakibul Islam <sakibul.islam0808@gmail.com>
2024-11-21 00:57:41 +0600

    Implement image upload and delete functionality in Manage Image modal

--- app/(private)/(with-sidebar)/super-admin/product/_components/manage-image-modal.jsx
+++ app/(private)/(with-sidebar)/super-admin/product/_components/manage-image-modal.jsx
@@ -1,14 +1,67 @@
 import Label from "@/components/shared/label";
+import {
+  useDeleteProductImageMutation,
+  useUploadProductImageMutation,
+} from "@/redux/api/productImageApi";
 import { Button, Modal, Upload } from "antd";
 import Dragger from "antd/es/upload/Dragger";
-import { ImageUp, Plus, Trash2 } from "lucide-react";
+import { ImageUp, Loader2, Plus, Trash2 } from "lucide-react";
 import Image from "next/image";
+import { useState } from "react";
+import { toast } from "sonner";
 
 export default function ManageImageModal({
   openManageImagesModal,
   setOpenManageImagesModal,
   currentProduct,
 }) {
+  const [uploadProductImage, { isLoading }] = useUploadProductImageMutation();
+  const [productType, setProductType] = useState(null);
+  const [deleteProductImage, { isLoading: isDeleteLoading }] =
+    useDeleteProductImageMutation();
+
+  const handleUploadImage = async (file, type) => {
+    if (!file || isLoading) return;
+
+    try {
+      setProductType(type);
+      const formData = new FormData();
+      formData.append("product_id", currentProduct.id);
+      formData.append("type", type);
+      formData.append("image", file.originFileObj);
+
+      await uploadProductImage(formData).unwrap();
+
+      toast.success("Image uploaded successfully");
+    } catch (error) {
+      toast.error(error.message || "Failed to upload image");
+    } finally {
+      setProductType(null);
+    }
+  };
+
+  const handleDeleteImage = async (imageId, type) => {
+    if (isDeleteLoading) return;
+
+    try {
+      setProductType(type);
+      await deleteProductImage(imageId).unwrap();
+      toast.success("Image deleted successfully");
+    } catch (error) {
+      toast.error(error.message || "Failed to delete image");
+    } finally {
+      setProductType(null);
+    }
+  };
+
+  const imageList = currentProduct?.images || [];
+
+  const primaryImage =
+    imageList && imageList.find((image) => image.type === "PRIMARY");
+
+  const secondaryImage =
+    imageList && imageList.find((image) => image.type === "SECONDARY");
+
   return (
     <Modal
       open={openManageImagesModal}
@@ -28,24 +81,24 @@ export default function ManageImageModal({
     >
       <>
         <div className="flex items-center gap-4">
-          {true ? (
+          {primaryImage?.image_url ? (
             <div>
               <Label required>Primary Image</Label>
               <div className="relative">
                 <Image
-                  src={
-                    "https://res.cloudinary.com/dl5rlskcv/image/upload/v1732000963/default-product_ilbqau.jpg"
-                  }
+                  src={primaryImage?.image_url}
                   alt=""
-                  width={1080}
-                  height={1080}
+                  width={128}
+                  height={128}
                   className="size-32 rounded object-cover"
                 />
                 <Button
                   size="small"
                   shape="circle"
-                  className="!absolute right-2 top-2"
+                  className="!disabled:cursor-not-allowed !absolute right-2 top-2"
                   danger
+                  onClick={() => handleDeleteImage(primaryImage.id, "PRIMARY")}
+                  disabled={isDeleteLoading && productType === "PRIMARY"}
                 >
                   <Trash2 size={16} />
                 </Button>
@@ -59,8 +112,13 @@ export default function ManageImageModal({
                 showUploadList={false}
                 accept="image/*"
                 maxCount={1}
-                // onChange={handleChange}
-                // disabled={isUpdateProfilePictureLoading || isUploading}
+                onChange={({ file }) => {
+                  if (file.status === "done" || file.status === "uploading") {
+                    handleUploadImage(file, "PRIMARY");
+                  }
+                }}
+                disabled={isLoading && productType === "PRIMARY"}
+                className="!disabled:cursor-not-allowed"
               >
                 <button
                   style={{
@@ -72,33 +130,44 @@ export default function ManageImageModal({
                     justifyContent: "center",
                     width: "100%",
                   }}
-                  className="!outline-none"
+                  className="!disabled:cursor-not-allowed !outline-none"
                 >
-                  <Plus size={24} />
-                  <div style={{ marginTop: 8 }}>Upload</div>
+                  {isLoading && productType === "PRIMARY" ? (
+                    <div className="gap-2text-sm flex size-32 flex-col items-center justify-center">
+                      <Loader2 size={16} className="animate-spin" />
+                      Uploading...
+                    </div>
+                  ) : (
+                    <>
+                      <Plus size={24} />
+                      <div style={{ marginTop: 8 }}>Upload</div>
+                    </>
+                  )}
                 </button>
               </Upload>
             </div>
           )}
 
-          {true ? (
+          {secondaryImage?.image_url ? (
             <div>
-              <Label required>Primary Image</Label>
+              <Label required>Secondary Image</Label>
               <div className="relative">
                 <Image
-                  src={
-                    "https://res.cloudinary.com/dl5rlskcv/image/upload/v1732000963/default-product_ilbqau.jpg"
-                  }
+                  src={secondaryImage?.image_url}
                   alt=""
-                  width={1080}
-                  height={1080}
+                  width={128}
+                  height={128}
                   className="size-32 rounded object-cover"
                 />
                 <Button
                   size="small"
                   shape="circle"
-                  className="!absolute right-2 top-2"
+                  className="!disabled:cursor-not-allowed !absolute right-2 top-2"
                   danger
+                  onClick={() =>
+                    handleDeleteImage(secondaryImage.id, "SECONDARY")
+                  }
+                  disabled={isDeleteLoading && productType === "SECONDARY"}
                 >
                   <Trash2 size={16} />
                 </Button>
@@ -112,8 +181,13 @@ export default function ManageImageModal({
                 showUploadList={false}
                 accept="image/*"
                 maxCount={1}
-                // onChange={handleChange}
-                // disabled={isUpdateProfilePictureLoading || isUploading}
+                onChange={({ file }) => {
+                  if (file.status === "done" || file.status === "uploading") {
+                    handleUploadImage(file, "SECONDARY");
+                  }
+                }}
+                disabled={isLoading && productType === "SECONDARY"}
+                className="!disabled:cursor-not-allowed"
               >
                 <button
                   style={{
@@ -125,10 +199,19 @@ export default function ManageImageModal({
                     justifyContent: "center",
                     width: "100%",
                   }}
-                  className="!outline-none"
+                  className="!disabled:cursor-not-allowed !outline-none"
                 >
-                  <Plus size={24} />
-                  <div style={{ marginTop: 8 }}>Upload</div>
+                  {isLoading && productType === "SECONDARY" ? (
+                    <div className="gap-2text-sm flex size-32 flex-col items-center justify-center">
+                      <Loader2 size={16} className="animate-spin" />
+                      Uploading...
+                    </div>
+                  ) : (
+                    <>
+                      <Plus size={24} />
+                      <div style={{ marginTop: 8 }}>Upload</div>
+                    </>
+                  )}
                 </button>
               </Upload>
             </div>
@@ -148,8 +231,8 @@ export default function ManageImageModal({
                       "https://res.cloudinary.com/dl5rlskcv/image/upload/v1732000963/default-product_ilbqau.jpg"
                     }
                     alt=""
-                    width={1080}
-                    height={1080}
+                    width={128}
+                    height={128}
                     className="w-full rounded object-cover sm:size-32"
                   />
                   <Button
