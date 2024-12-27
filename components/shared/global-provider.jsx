6dedd725f1fed210622de3c6dad5168c140bc8ba
Md Sakibul Islam <sakibul.islam0808@gmail.com>
2024-12-27 15:29:41 +0600

    Refactor Firebase integration: replace old messaging service with new token management and notification handling

--- components/shared/global-provider.jsx
+++ components/shared/global-provider.jsx
@@ -1,43 +1,17 @@
 "use client";
 
+import useFcmToken from "@/hooks/use-fcm-token";
 import { store } from "@/redux/store";
 import themeConfig from "@/theme/themeConfig";
-import { requestFCMToken } from "@/utils/fireabse";
 import { AntdRegistry } from "@ant-design/nextjs-registry";
 import { ConfigProvider } from "antd";
-import { Loader2 } from "lucide-react";
-import { useEffect, useState } from "react";
 import { Provider } from "react-redux";
 import { Toaster } from "sonner";
 
 export default function GlobalProvider({ children }) {
-  const [fcmToken, setFcmToken] = useState(null);
-  const [loading, setLoading] = useState(true);
+  const { token } = useFcmToken();
 
-  useEffect(() => {
-    const fetchFCMToken = async () => {
-      try {
-        const token = await requestFCMToken();
-        setFcmToken(token);
-      } catch (error) {
-        console.error("Error fetching FCM token: ", error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchFCMToken();
-  }, []);
-
-  if (loading) {
-    return (
-      <div className="flex h-screen items-center justify-center">
-        <Loader2 className="animate-spin" size={48} />
-      </div>
-    );
-  }
-
-  console.log("FCM Token: ", fcmToken);
+  console.log("FCM Token: ", token);
 
   return (
     <Provider store={store}>
---
+++ fireabse.js
@@ -0,0 +1,38 @@
+import { getApp, getApps, initializeApp } from "firebase/app";
+import { getMessaging, getToken, isSupported } from "firebase/messaging";
+
+// Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDjkdfTY8qQIRuCam8AmPZE-2hM1Cu6jx0",
+  authDomain: "let-z-gear.firebaseapp.com",
+  projectId: "let-z-gear",
+  storageBucket: "let-z-gear.firebasestorage.app",
+  messagingSenderId: "769656323364",
+  appId: "1:769656323364:web:0a3fc2b6af78073fb007b1",
+  measurementId: "G-46MV4ESWNQ",
+};
+
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
+
+const messaging = async () => {
+  const supported = await isSupported();
+  return supported ? getMessaging(app) : null;
+};
+
+export const fetchToken = async () => {
+  try {
+    const fcmMessaging = await messaging();
+    if (fcmMessaging) {
+      const token = await getToken(fcmMessaging, {
+        vapidKey: process.env.NEXT_PUBLIC_FIREBASE_FCM_VAPID_KEY,
+      });
+      return token;
+    }
+    return null;
+  } catch (err) {
+    console.error("An error occurred while fetching the token:", err);
+    return null;
+  }
+};
+
+export { app, messaging };
