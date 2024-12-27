465c023b562d4ae441a08ce3a6b843ba2d1c9e56
Md Sakibul Islam <sakibul.islam0808@gmail.com>
2024-12-27 14:30:12 +0600

    Add Firebase integration for push notifications and FCM token management

--- components/shared/global-provider.jsx
+++ components/shared/global-provider.jsx
@@ -2,12 +2,43 @@
 
 import { store } from "@/redux/store";
 import themeConfig from "@/theme/themeConfig";
+import { requestFCMToken } from "@/utils/fireabse";
 import { AntdRegistry } from "@ant-design/nextjs-registry";
 import { ConfigProvider } from "antd";
+import { Loader2 } from "lucide-react";
+import { useEffect, useState } from "react";
 import { Provider } from "react-redux";
 import { Toaster } from "sonner";
 
 export default function GlobalProvider({ children }) {
+  const [fcmToken, setFcmToken] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchFCMToken = async () => {
+      try {
+        const token = await requestFCMToken();
+        setFcmToken(token);
+      } catch (error) {
+        console.error("Error fetching FCM token: ", error);
+      } finally {
+        setLoading(false); // Ensure loading state is updated regardless of success or failure
+      }
+    };
+
+    fetchFCMToken();
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="flex h-screen items-center justify-center">
+        <Loader2 className="animate-spin" size={48} />
+      </div>
+    );
+  }
+
+  console.log("FCM Token: ", fcmToken);
+
   return (
     <Provider store={store}>
       <Toaster position="top-center" richColors />
---
+++ firebase-messaging-sw.js
@@ -0,0 +1,24 @@
+importScripts(
+  "https://www.gstatic.com/firebasejs/10.13.2/firebase-app-compat.js",
+);
+importScripts(
+  "https://www.gstatic.com/firebasejs/10.13.2/firebase-messaging-compat.js",
+);
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDjkdfTY8qQIRuCam8AmPZE-2hM1Cu6jx0",
+  authDomain: "let-z-gear.firebaseapp.com",
+  projectId: "let-z-gear",
+  storageBucket: "let-z-gear.firebasestorage.app",
+  messagingSenderId: "769656323364",
+  appId: "1:769656323364:web:0a3fc2b6af78073fb007b1",
+  measurementId: "G-46MV4ESWNQ",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function (payload) {
+  console.log("Received background message ", payload);
+});
